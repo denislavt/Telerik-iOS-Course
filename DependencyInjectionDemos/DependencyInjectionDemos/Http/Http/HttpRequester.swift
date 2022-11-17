ffd328ad713cda29b90954787f65853112ba2230
Denislav Todorov <remsaka@yahoo.com>
2022-11-17 17:20:50 +0200

    Dependency Injection Demos
    http added

---
+++ DependencyInjectionDemos/DependencyInjectionDemos/Http/Http/HttpRequester.swift
@@ -0,0 +1,19 @@
+//
+//  HttpRequester.swift
+//  DependencyInjectionDemos
+//
+//  Created by Denislav Todorov on 17.11.22.
+//
+
+import Foundation
+
+
+class HttpRequester {
+    func get(fromUrl urlString: String, withHeaders headers: Dictionary<String, String>?){
+        
+    }
+    
+    func post(toUrl urlString: String,withBody: Any, withHeaders headers: Dictionary<String, String>?){
+        
+    }
+}
---
+++ DependencyInjectionDemos/DependencyInjectionDemos/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DependencyInjectionDemos
+//
+//  Created by Denislav Todorov on 17.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var data: Any?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
